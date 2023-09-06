d61f4decdfaa6f1d679fc3971cf20d799bbd5518
moicr96 <127130892+moicr96@users.noreply.github.com>
2023-09-05 20:48:22 -0600

    Se mejora la organización

---
+++ 10 days of javascript/17_Day 5 Arrow Functions.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    for (let i = 0; i <=nums.length-1; i++) {
+        if (nums[i]%2==0) {
+            
+            nums[i]=nums[i]*2
+        }else{
+            nums[i]=nums[i]*3
+        }
+    }
+    return(nums);
+} 
+/*
+otra forma:
+function modifyArray(nums) {
+return nums.map(elm => (elm % 2 == 0) ? elm*2 : elm*3);    
+}
+*/
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
